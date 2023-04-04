790e66fdeb9fd15c5558fc0ffd4de0b42be740cc
JennyPersichini <nenny1096@gmail.com>
2023-04-04 15:00:04 +0200

    1st step

---
+++ main.js
@@ -0,0 +1,18 @@
+const listaSpesa = ['pane', 'riso', 'pasta', 'farina', 'uova', 'latte'];
+
+//ciclo for
+for (let i = 0; i < listaSpesa.length; i++){
+
+    console.log(listaSpesa[i]);
+
+}
+
+//ciclo while
+let i = 0;
+
+while (i < listaSpesa.length){
+
+    console.log(listaSpesa[i]);
+    i++;
+
+}
